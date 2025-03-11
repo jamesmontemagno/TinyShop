c531581c034891a7db99e0014b180a61e94f67a8
Jon Galloway <jongalloway@gmail.com>
2025-03-11 01:43:14 -0700

    Added PDF injestor

--- Products/Program.cs
+++ Products/Program.cs
@@ -1,11 +1,15 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.AI;
-using OpenAI;
-using Products.Data;
-using Products.Endpoints;
 using System.ClientModel;
 using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.VectorData;
 using OllamaSharp;
+using OpenAI;
+using OpenAI.VectorStores;
+using Products.Data;
+using Products.Endpoints;
+using Products.Services;
+using Products.Services.Ingestion;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,8 +23,29 @@ builder.AddOllamaApiClient("chat")
 builder.Services.AddDbContext<ProductDataContext>(options =>
     options.UseInMemoryDatabase("inmemproducts"));
 
-// Add services to the container.
+var credential = new ApiKeyCredential(builder.Configuration["GitHubModels:Token"] ?? throw new InvalidOperationException("Missing configuration: GitHubModels:Token. See the README for details."));
+var openAIOptions = new OpenAIClientOptions()
+{
+    Endpoint = new Uri("https://models.inference.ai.azure.com")
+};
+
+var ghModelsClient = new OpenAIClient(credential, openAIOptions);
+var chatClient = ghModelsClient.AsChatClient("gpt-4o-mini");
+var embeddingGenerator = ghModelsClient.AsEmbeddingGenerator("text-embedding-3-small");
+
+var vectorStore = new JsonVectorStore(Path.Combine(AppContext.BaseDirectory, "vector-store"));
+
+builder.Services.AddSingleton<IVectorStore>(vectorStore);
+builder.Services.AddScoped<DataIngestor>();
+builder.Services.AddSingleton<SemanticSearch>();
+builder.Services.AddChatClient(chatClient).UseFunctionInvocation().UseLogging();
+builder.Services.AddEmbeddingGenerator(embeddingGenerator);
+
+builder.Services.AddDbContext<IngestionCacheDbContext>(options =>
+    options.UseSqlite("Data Source=ingestioncache.db"));
+
 var app = builder.Build();
+IngestionCacheDbContext.Initialize(app.Services);
 
 app.MapDefaultEndpoints();
 
@@ -34,6 +59,10 @@ app.UseOutputCache();
 app.UseStaticFiles();
 
 app.CreateDbIfNotExists();
+
+await DataIngestor.IngestDataAsync(
+    app.Services,
+    new PDFDirectorySource(Path.Combine(builder.Environment.WebRootPath, "data")));
 
 app.Run();
 
